ce4d9ba1a8a56cf9e00903d0b4ebd0d7da00c523
ElenaBakova <lenabakova72@gmail.com>
2025-01-17 19:09:46 +0300

    Add Loader

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,21 @@
+import './Loader.css';
+
+const Loader = () => {
+    return (
+        <div className="loader-wrapper">
+            <div className="loader">
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+            </div>
+            <p style={{fontSize: "18px", color:"#2196F3"}}>Загружаем котиков</p>
+            <div className="loader">
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+            </div>
+        </div>
+    );
+};
+
+export default Loader;
