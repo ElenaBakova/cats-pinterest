50c84b3f0928ebbb372092422d24b7b203c38aaa
ElenaBakova <lenabakova72@gmail.com>
2025-01-20 15:21:44 +0300

    Change BrowserRouter to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,17 @@ import "./styles/App.css";
 import Navbar from "./components/Navbar/Navbar.tsx";
 import Main from "./components/Main.tsx";
 
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import FavouritesProvider from "./contexts/FavouritesContext/FavouritesProvider.tsx";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Navbar />
       <FavouritesProvider>
         <Main />
       </FavouritesProvider>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
